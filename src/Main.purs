96aac99b1d9cc8e4eae95f42bf71d59d242e1aa8
Sam Marshall <sjmarshy@gmail.com>
2017-06-28 22:33:37 +0100

    exercise 2.2

--- src/Main.purs
+++ src/Main.purs
@@ -4,31 +4,45 @@ import Prelude
 
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Data.Maybe (Maybe(..))
 
-data NumberTree
+data Element a = Element a
+data NumberTree a
   = Empty 
-  | Node 
-    { left :: NumberTree
-    , el :: Int
-    , right :: NumberTree
-    }
+  | Node (NumberTree a) (Element a) (NumberTree a)
 
-instance treeShow :: Show NumberTree where
+instance showElement :: (Show a) => Show (Element a) where
+  show (Element a) = show a
+
+instance showNumberTree :: (Show a) => Show (NumberTree a) where
   show Empty = "Empty"
-  show (Node { left: left, el: el, right: right }) = "Node( " <> show left <> " " <> show el <> " " <> show right <> " )"
+  show (Node left el right) = "Node( " <> show left <> " " <> show el <> " " <> show right <> " )"
 
-empty :: NumberTree -> Boolean
+empty :: forall a. NumberTree a -> Boolean
 empty Empty = true
 empty _ = false
 
-raw :: NumberTree -> Int -> NumberTree -> NumberTree
-raw left el right = Node { left: left, el: el, right: right }
+raw :: forall a. NumberTree a -> a -> NumberTree a -> NumberTree a
+raw left el right = Node left (Element el) right
 
-insert :: Int -> NumberTree -> NumberTree
+member :: forall a. (Ord a) => NumberTree a -> a -> Maybe a -> Boolean
+member Empty _ _ = false
+member (Node Empty (Element el) Empty) mem (Just test) = mem == el || test == el
+member (Node Empty (Element el) Empty) mem Nothing = mem == el
+member (Node left (Element el) right) mem t =
+  if mem < el
+  then member left mem t
+  else member right mem (Just el)
+
+insert :: forall a. (Ord a) => a -> NumberTree a -> NumberTree a
 insert n Empty = raw Empty n Empty
-insert n (Node { left: left, el: el, right: right }) =
-  if n < el then raw (insert n left) el right else raw left el (insert n right)
+insert n (Node left (Element el) right) =
+  if n < el 
+  then raw (insert n left) el right 
+  else raw left el (insert n right)
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
   log $ show $ insert 10 $ insert 8 Empty
+  log $ show $ member (insert 10 $ insert 9 $ insert 8 Empty) 10 Nothing
+  
